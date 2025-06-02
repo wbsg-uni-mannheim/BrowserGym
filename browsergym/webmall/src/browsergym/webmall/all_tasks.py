099253f552ff5b0bb36f38bacd053c877f2bf7a7
Wifo5: Hiwi (31.12.2025) <jcaspary@dws-login-02.informatik.uni-mannheim.de>
2025-06-02 18:36:08 +0200

    Revert "feat: initial webmall scripts"
    This reverts commit dbe4db18fefb11e1aa44c2f637ce14ebd583c5d3.

--- browsergym/webmall/src/browsergym/webmall/all_tasks.py
+++
@@ -1,8 +0,0 @@
-from .task import WebMallTask
-
-class SingleProductSearch(WebMallTask):
-    def __init__(self, seed: int = None):
-        super().__init__(seed=seed, task_id="Webmall_Single_Product_Search_Task1")
-
-
-all_tasks = [SingleProductSearch]
